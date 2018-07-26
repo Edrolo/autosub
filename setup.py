139bfe655dcebe1ffe8a02ef0544f1bef8d221ab
Matt Fisher <m@ttfisher.com>
2018-07-26 15:49:06 +1000

    Bump versions

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ long_description = (
 
 setup(
     name='autosub',
-    version='0.4.0b5',
+    version='0.4.0b6',
     description='Auto-generates subtitles for any video or audio file',
     long_description=long_description,
     author='Anastasis Germanidis',
